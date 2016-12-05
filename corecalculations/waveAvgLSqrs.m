df21a01e4888c714436b9ca6d5f022b3af35d058
olavobm <olavobm@gmail.com>
2016-12-05 15:25:35 -0800

    waveAvgLSqrs.m: remove polynomial fit implemented for no window case. There is still room for improving efficiency.

--- corecalculations/waveAvgLSqrs.m
+++ corecalculations/waveAvgLSqrs.m
@@ -1,10 +1,14 @@
-function [davg, xavg] = waveAvgLSqrs(x, d, prd, wnd, wndptslide)
+function [davg, xavg] = waveAvgLSqrs(x, d, prd, porder, wnd, wndptslide)
 % [davg, xavg] = WAVEAVGLSQRS(x, d, prd, wnd, wndptslide)
 % 
 %   inputs:
 %       - x: vector or matrix.
 %       - d: variable to be wave-averaged (each of its rows).
-%       - prd: period of the sinusoidal, in the same units as x.
+%       - prd: period of the sinusoidal (or vector of periods), in
+%              the same units as x.
+%       - porder (optional): order of polynomials to fit, rather than just
+%                            fitting a constant, such that davg is the mean
+%                            of the polynomial fit.
 %       - wnd (optional): window to, in the same units as x.
 %       - wndptslide (optional): this can only be input if wnd is.
 %
@@ -29,7 +33,6 @@ function [davg, xavg] = waveAvgLSqrs(x, d, prd, wnd, wndptslide)
 
 nrows = size(d, 1);
 
-
 % Create the indices to call the rows of x. If it is a vector (1 row),
 % indrx is a vector of 1's, otherwise it is a vector with its rows:
 if isvector(x) || iscolumn(x)
@@ -50,6 +53,9 @@ else
     
 end
 
+%
+xncols = size(x, 2);
+
 
 %% Check whether wnd and wndptslide were specified appropriately:
 
@@ -68,8 +74,17 @@ end
 % Constant:
 imfWave.power = 0;
 
-% Sinusoidal with frequency 1/prd:
-imfWave.sine = 1/prd;
+% Sinusoidal(s) with frequency(ies) 1./prd:
+imfWave.sine = 1./prd;
+
+%
+if ~exist('porder', 'var') || isempty(porder)
+else
+    if iscolumn(porder)
+        porder = porder';
+    end
+    imfWave.power = [imfWave.power porder];
+end
 
 
 %% Do the fit:
@@ -86,8 +101,26 @@ if ~exist('wnd', 'var')
         % Fit:
         [~, wvavg_aux] = myleastsqrs(x(indrx(i), :), d(i, :), imfWave);
 
-        % Assign the mean of the sinusoidal to the output variable:
-        davg(i, 1) = wvavg_aux(1);
+        %
+        if ~exist('porder', 'var')        
+            % Just assign the constant from the fit to the output variable:
+            davg(i, 1) = wvavg_aux(1);
+        else
+            % In this case, reconstruct the polynomial part of the
+            % fit on a higher resolution grid and take the mean:
+            nauxgrid = 2*xncols;
+            x2G = linspace(x(indrx(i), 1), x(indrx(i), end), nauxgrid);
+            x2G = x2G';
+            x2G = repmat(x2G, 1, length(imfWave.power));
+            
+            Gaux = x2G .^ repmat(imfWave.power, nauxgrid, 1);
+            
+            davg(i, 1) = mean(Gaux*wvavg_aux(1:length(imfWave.power)));
+            
+            % if x is just a vector, the creation of Gaux can be done
+            % before the loop.
+        end
+        
     end
 
     % Create the mean time output: 
@@ -95,9 +128,9 @@ if ~exist('wnd', 'var')
     
 % If there is a window, use sliding harmonic fit to wave-average:
 else
-    
+
     [davg, xavg] = sliding_harmonicfit(x, d, wnd, wndptslide, ...
-                                                    imfWave, [true false]);
+                                            imfWave, [true, false, false]);
     
 end
 
