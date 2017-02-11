ba6d08a2ef7de5aefbf6cc817d2fad15cc8b9623
olavobm <olavobm@gmail.com>
2017-02-10 16:04:39 -0800

    first commit.

---
+++ corecalculations/wkbNorm.m
@@ -0,0 +1,33 @@
+function [zn, xn] = wkbNorm(z, N, x, N0)
+% [zn, xn] = WKBNORM(z, N, x)
+%
+%   inputs:
+%       - z:
+%       - N: buyancy frequency, in radians per second.
+%       - x:
+%       - N0 (optional): reference buyancy frequency
+%                        (default is the average).
+%
+%   outputs:
+%       - zn: stretched vertical coordinate.
+%       - xn: WKB-normalized x.
+%
+% WKBNORM does the normalization of ....
+%
+% What do I really need???
+% Velocity only???
+% How small can N really be??
+%
+%
+% Olavo Badaro Marques, 10/Feb/2017.
+
+
+%%
+
+
+%%
+
+xn = x ./ ;
+
+
+%%
