a1cf3b82d235e93b77e5e3ae4f1adc86b9a128e3
olavobm <olavobm@gmail.com>
2017-06-13 23:03:46 -0700

    included normalization factor as input parameter.

--- uGM76.m
+++ uGM76.m
@@ -1,6 +1,13 @@
-function specGM = uGM76(f0, N0, fgrid)
+function specGM = uGM76(f0, N0, fgrid, freqFac)
+% specGM = UGM76(f0, N0, fgrid)
 %
+%   inputs:
+%       - f0: Coriolis parameter (radians per second).
+%       - N0: bouyancy frequency (radians per second).
+%       - fgrid: frequency (radians per second).
 %
+%   outputs:
+%       - specGM:
 %
 % Olavo Badaro, 12/Jun/2017.
 
@@ -17,10 +24,31 @@ params = Gm76Params;
 S = GmOm('Vel', fgrid, f0, N0, params);
 
 
+%%
+
+if ~exist('freqFac', 'var')
+    
+    freqFac = 1;
+    
+else
+    
+    if ischar(freqFac)
+    
+        if strcmp(freqFac, 'cpd')
+            freqFac = (24*3600)/(2*pi);
+        end
+    
+    else
+
+
+    end
+    
+end
+
+
+
 %% Convert frequency vector from radians
 % per second to cycles per day:
 
-freqFac = (24*3600)/(2*pi);
-
-specGM.fvec = fgrid * freqFac;
+specGM.freq = fgrid * freqFac;
 specGM.spec = S / freqFac;
