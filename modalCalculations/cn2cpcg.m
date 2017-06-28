a8acace165681acc76c3434676afadc482a7560b
olavobm <olavobm@gmail.com>
2017-06-28 15:14:13 -0700

    now working.

--- modalCalculations/cn2cpcg.m
+++ modalCalculations/cn2cpcg.m
@@ -1,27 +1,36 @@
-function [cp, cg] = cn2cpcg(ce, lat)
-% [cp, cg] = CN2CPCG(ce, lat)
+function [cp, cg] = cn2cpcg(cn, freq, lat)
+% [cp, cg] = CN2CPCG(cn, freq, lat)
 %
 %   inputs:
-%       - cn:
-%       - lat:
+%       - cn: eigenspeed of the n'th vertical mode.
+%       - freq: wave frequency (in radians per second).
+%       - lat: latitude (in degrees).
 %
 %   outputs:
-%       - cp: horizontal phase speed.
-%       - cg: horizontal group velocity magnitude.
+%       - cp: horizontal phase speed (in m/s).
+%       - cg: horizontal group velocity magnitude (in m/s).
 %
-% The eigenspeed is the geometric mean of phase speed and group velocity
-% magnitude. Without rotation, a shallow-water gravity wave has
-% horizontal.... phase speed and group velocity magnitude are equal.
-% With rotation such that
-%
-%   cp = 
-%
-%
-%   cg = 
+% The eigenspeed is the geometric mean of phase speed (cp) and group
+% velocity (cg) magnitude. Without rotation, cp and cg have the same
+% magnitude (i.e. due to the shallow-water approximation).
 %
 % Olavo Badaro Marques, 28/Jun/2017.
 
 
-cgall(:, i) = IWM2.ce' .* ( (2*pi/(12.42*3600))^2 - ...
-                                (sw_f(IWM2.lat))^2 ) /  ...
-                                ((2*pi/(12.42*3600))^2);
+%% Calculate Coriolis parameter:
+
+omegaEarth = 7.292115e-5;	% Earth's rotation rate
+
+f0 = 2 * omegaEarth * sin(lat);
+
+
+%% Compute phase speed and group velocity magnitude:
+
+%
+cp = cn .* freq/(sqrt(freq.^2 - f0.^2));
+
+%
+cg = cn .* sqrt(freq.^2 - f0.^2)/freq;
+
+
+
