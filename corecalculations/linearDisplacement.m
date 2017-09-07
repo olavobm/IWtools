42bd3fce3b21a1c2e67fd61e7c32587dd926f6ea
olavobm <olavobm@gmail.com>
2017-09-06 20:12:10 -0700

    edited comment.

--- corecalculations/linearDisplacement.m
+++ corecalculations/linearDisplacement.m
@@ -25,7 +25,7 @@ function eta = linearDisplacement(zxp, xp, zxB, xB, cutoff)
 % Olavo Badaro Marques, 02/05/2017.
 
 
-%% If no cutoff input is given, then select default value:
+%% If no cutoff input is given, then select default value
 
 if ~exist('cutoff', 'var')
     cutoff = 0;
@@ -34,34 +34,34 @@ end
 
 %% Take the sign difference of the top to bottom difference,
 % this will be used to determine if a positive anomaly
-% corresponds to a positive or negative displacement:
+% corresponds to a positive or negative displacement
 
 gradSign = sign(zxB(end) - zxB(1));
 
 
-%% Take the derivative of xB:
+%% Take the derivative of xB
 
 dxBdz = (xB(2:end) - xB(1:end-1)) ./ diff(zxB);
 
 zdxBdz = (zxB(1:end-1) + zxB(2:end))/2;
 
 
-%%
+%% Interpolate the derivative of xB onto the same grid (zxp) as xp
 
-xBatXp = interp1(zdxBdz, dxBdz, zxp);
+dxBatXp = interp1(zdxBdz, dxBdz, zxp);
     
-xBatXp = repmat(xBatXp, 1, size(xp, 2));
+dxBatXp = repmat(dxBatXp, 1, size(xp, 2));
 
 
-%% Compute vertical displacement:
+%% Compute vertical displacement
 
-eta = xp ./ xBatXp;
+eta = xp ./ dxBatXp;
 
 
-%% Final modifications to eta:
+%% Final modifications to eta
 
 % Remove values associated with dXBdz below cutoff:
-lcut = (abs(xBatXp) < abs(cutoff));
+lcut = (abs(dxBatXp) < abs(cutoff));
 eta(lcut) = NaN;
 
 % Multiply eta by -1 or + 1 depending on whether xB
