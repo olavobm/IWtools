85f60d2d0a148e1242ab7a836744a5256c817430
olavobm <olavobm@gmail.com>
2017-09-20 16:19:35 -0700

    implemented velocity and pressure calculations.

--- modeflux2var.m
+++ modeflux2var.m
@@ -1,4 +1,4 @@
-function [varout] = modeflux2var(F, D, N2, freq, lat0, mdn, listvar)
+function [varout] = modeflux2var(F, D, N2, freq, lat0, nmd, listvar)
 % [varout] = MODEFLUX2VAR(F, D, N2, freq, lat0, nmd, listvar)
 %
 %   inputs
@@ -7,7 +7,7 @@ function [varout] = modeflux2var(F, D, N2, freq, lat0, mdn, listvar)
 %       - N2: buoyancy frequency squared (radians per second squared).
 %       - freq: wave frequency (cycles per day).
 %       - lat0: latitude.
-%       - mdn (optional): mode number (default is 1).
+%       - nmd (optional): mode number (default is 1).
 %       - listvar (optional): cell array with string of
 %                             variables to look at (default is u).
 %
@@ -17,34 +17,62 @@ function [varout] = modeflux2var(F, D, N2, freq, lat0, mdn, listvar)
 %
 % Result for constant buoyancy frequency.
 %
+%   TO DO:
+%       - clean the code.
+%       - add comments.
+%
 % Olavo Badaro Marques, 20/Sep/2017.
 
 
 %%
 
-if ~exist('mdn', 'var') || isempty(mdn)
-	mdn = 1;
+if ~exist('mdn', 'var') || isempty(nmd)
+	nmd = 1;
 end
 
 if ~exist('listvar', 'var')
 	listvar = {'u'};
 end
 
+%
+rho0 = 1025;
+
+
+%%
+
+f0 = gsw_f(lat0);
+
+
+%%
+
+freq = ((2*pi)/(24*3600)) * freq;
+
 
 %% 
 
-for i1 = 1:length(mdn)
+for i1 = 1:length(nmd)
     
+    %
+    kmag = (nmd*(pi/D)) * sqrt( (freq^2 - f0^2)/(N2 - freq^2) );
+    
+    %
     for i2 = 1:length(listvar)
         
         switch listvar{i2}
             
             case 'u'
                 
+                varout = F * 4 * rho0 * (freq^2 - f0^2) / (freq * kmag * D);
                 
+                varout = sqrt(varout);
+                
+                varout = varout * ((freq*kmag) / (rho0 * (freq^2 - f0^2)));
                 
             case 'p'
                 
+                varout = F * 4 * rho0 * (freq^2 - f0^2) / (freq * kmag * D);
+                
+                varout = sqrt(varout);
                 
         end
         
