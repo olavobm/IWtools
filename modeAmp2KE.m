335065132d8426a7810df7cbcfa8b83c6b150a91
olavobm <olavobm@gmail.com>
2017-09-22 15:52:39 -0700

    edited header.

--- modeAmp2KE.m
+++ modeAmp2KE.m
@@ -1,25 +1,34 @@
 function depthintKE = modeAmp2KE(uamp, vamp, H, ntype)
-% depthintKE = MODEAMP2KE(uamp, vamp, H, ltype)
+% depthintKE = MODEAMP2KE(uamp, vamp, H, ntype)
 %
 %   inputs
-%       - uamp:
-%       - vamp:
-%       - H:
-%       - ntype (optional): default is 0.
+%       - uamp: u modal amplitude amplitude.
+%       - vamp: v   "       "         "
+%       - H: water depth (in meters).
+%       - ntype (optional): normalization type of the modal
+%                           shapes (default is 0).
 %
 %   outputs
-%       - depthintKE: depth0integrated kinetic energy (in J/m^2).
+%       - depthintKE: time-averaged, depth-integrated
+%                     kinetic energy (in J/m^2).
 %
+% Compute time-averaged, depth-integrated Kinetic Energy from the modal
+% amplitude of velocity components. The time-average value is NOT a 
+% function of the phase difference between the components.
 %
-% case 0 - standard normalization (depth integral
-%          squared is Kronecker delta)
-% case 1 - cte N2 and modal shapes are unscaled sines and cosines.
-%
+% When taking the depth integral, there is a constant factor that
+% multiplies the velocity squared that comes from the the integral
+% of the modal shapes. The type of normalization is specified by
+% the optional input "ntype". The avalable possibilities are:
+% 
+%       * ntype == 0 - standard normalization (depth integral
+%                      of modal shaped squared is one).
+%       * ntype == 1 - cte N2 and modal shapes are unscaled cosines.
 %
 % Olavo Badaro Marques, 21/Sep/2017.
 
 
-%%
+%% Check inputs, set constant
 
 if ~exist('ntype', 'var')
 	ntype = 0;
@@ -29,7 +38,7 @@ end
 rho0 = 1025;
 
 
-%%
+%% Define constant factor based on the type of normalization
 
 switch ntype
      
@@ -41,12 +50,10 @@ switch ntype
         
         kefactor = H/2;
         
-% % %     case 2
-% % %         
-% % %         kefactor = trapz(zmod, ModShape.^2);
 end
 
 
-%% Compute depth-integrated KE
+%% Compute depth-integrated, time-averaged KE
 
-depthintKE = (rho0/2) * kefactor .* (uamp.^2 + vamp.^2);
+depthintKE = (1/2) * (rho0/2) * kefactor .* (uamp.^2 + vamp.^2);
+
