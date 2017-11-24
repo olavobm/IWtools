bac299fd15798841b4600668fe0f370a89c5cbd0
olavobm <olavobm@gmail.com>
2017-11-24 14:53:12 -0800

    edited comments and removed a possible call for tidalFreq.m.

--- ellipseLat0.m
+++ ellipseLat0.m
@@ -10,28 +10,29 @@ function vurto = ellipseLat0(lat0, wvfreq)
 %                velocity components perpendicular and parallel
 %                to wave propoagation.
 %
+% From the equations of motion, inertia-gravity waves (barotropic or
+% each baroclinic mode) have v/u = -i*f0/wvfreq -- which describes
+% clockwise particle trajectories in the northern hemisphere. In this
+% notation, u is horizontal velocity component in the direction of
+% wave propagation.
 %
 % Dependecies: gsw_f.m.
 %
 % Olavo Badaro Marques, 21/Nov/2017.
 
 
-%%
+%% Computes the Coriolis parameter
 
 f0 = gsw_f(lat0);
 
 
-%%
-
-if isa(wvfreq, 'char')
-    wvfreq = tidalFreq(wvfreq);
-end
+%% Convert wave frequency from cycles per day to radians per second
 
 wvfreq = (2*pi/(24*3600))* wvfreq;
 
 
-
-%%
+%% Compute the magnitude of the ratio between
+% amplitude of the velocity components
 
 vurto = abs(f0) ./ wvfreq;
 
