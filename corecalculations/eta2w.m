974b35f93c74177ad26d8cd6ccaac4bff74534b2
olavobm <olavobm@gmail.com>
2018-04-24 15:10:39 -0700

    calculation is a simple differentiation.

--- corecalculations/eta2w.m
+++ corecalculations/eta2w.m
@@ -1,12 +1,13 @@
-function w = eta2w(t, eta)
-% w = ETA2W(t, eta)
+function [w, midtime] = eta2w(time, eta)
+% [w, midtime] = ETA2W(time, eta)
 %
 %   inputs
-%       - t: time
+%       - time: time
 %       - eta: displacement
 %
 %   outputs
 %       - w: d(eta)/dt
+%       - midtime:
 %
 %
 %
@@ -15,3 +16,17 @@ function w = eta2w(t, eta)
 %
 % Olavo Badaro Marques, 24/Apr/2018.
 
+
+%%
+
+if isvector(time) && ~isvector(eta)
+	time = repmat(time, size(eta, 1), 1);
+end
+
+%
+midtime = (time(:, 1:end-1) - time(:, 2:end)) ./ 2;
+
+
+%%
+w = (eta(:, 2:end)  - eta(:, 1:end-1)) ./ ...
+    (time(:, 2:end) - time(:, 1:end-1));
